1e2035e9fa9dbb723b396fb0501935baf7991f49
Jakub <jakubsopel7@gmail.com>
2017-07-28 17:43:28 +0200

    Fixing and cleaning up the code

--- main.cpp
+++ main.cpp
@@ -14,15 +14,32 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(width, height), "Gra");
 	window.setFramerateLimit(60);
 
-	//player
+	 //player
 	sf::Texture player;
 	player.loadFromFile("statek.png");
-	// Create a sprite
 	sf::Sprite sprite1;
 	sprite1.setTexture(player);
 	sprite1.setTextureRect(sf::IntRect(0, 0, 108, 126));
 	//end of player
 
+	//background
+	sf::Texture texture;
+	texture.loadFromFile("tlo.png");
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setPosition(0, 0);
+    //end of background
+
+    //BORDER SQUARES FOR COLLISION DETECTION
+	sf::RectangleShape r1(sf::Vector2f(100, height));
+	r1.setPosition(-100, 0);
+	sf::RectangleShape r2(sf::Vector2f(100, height));
+	r2.setPosition(width, 0);
+	sf::RectangleShape r3(sf::Vector2f(width, 3));
+	r3.setPosition(0, -3);
+	sf::RectangleShape r4(sf::Vector2f(width, 3));
+	r4.setPosition(0, height+3);
+	//END OF BORDER SQUARES
 
 	//muzyka
 	/*
@@ -32,25 +49,15 @@ int main()
 	music.play();
 	*/
 
-	////////////////////
 
-	// Declare and load a texture
-	sf::Texture texture;
-	texture.loadFromFile("tlo.png");
-	// Create a sprite
-	sf::Sprite sprite;
-	sprite.setTexture(texture);
-	sprite.setPosition(0, 0);
-
-	kwadraty(width,height);
-
-	sf::RectangleShape rect(sf::Vector2f(20, 20));
-	rect.setFillColor(sf::Color::Red);
+	//SPEED OF PLAYER MOVEMENT
 	float moveSpeed = 800.0f;
-
+    //END OF PLAYER MOVEMENT SPEED
 
 	sf::Clock clock;
 
+
+    //------MAIN GAME LOOP-------------
 	while (window.isOpen())
 	{
 
@@ -62,10 +69,35 @@ int main()
 			case sf::Event::Closed:
 				window.close();
 				break;
+
 			}
 		}
 
-		klawiatura();
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                {
+                   sprite1.move(moveSpeed * clock.getElapsedTime().asSeconds(), 0);
+                    if (sprite1.getGlobalBounds().intersects(r2.getGlobalBounds()))
+                        sprite1.move(-20.0, 0.0);
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                {
+                    sprite1.move(-moveSpeed * clock.getElapsedTime().asSeconds(), 0);
+                    if (sprite1.getGlobalBounds().intersects(r1.getGlobalBounds()))
+                        sprite1.move(20.0, 0.0);
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+                {
+                    sprite1.move(0 * clock.getElapsedTime().asSeconds(), -10);
+                    if (sprite1.getGlobalBounds().intersects(r3.getGlobalBounds()))
+                        sprite1.move(0.0, 20.0);
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+                {
+                    sprite1.move(0 * clock.getElapsedTime().asSeconds(), 10);
+                    if (sprite1.getGlobalBounds().intersects(r4.getGlobalBounds()))
+                        sprite1.move(0.0, -20.0);
+                }
 
 		sf::Time time = clock.getElapsedTime();
 		cout << 1.0f / time.asSeconds() << endl;
@@ -81,4 +113,5 @@ int main()
 	}
 	return 0;
 }
+
 
